111dd97df5d166933e8180ef176900305f12ce4c
Tor Alver Moen <tor.alver.moen@gmail.com>
2023-10-17 11:35:31 +0200

    Added Registration Page View function
    #jira GT3-5

--- pages/registrationPage/registrationPageView.js
+++ pages/registrationPage/registrationPageView.js
@@ -1,4 +1,19 @@
-function registrationPageView(){
+function registrationPageView() {
+    // TOR: Is it ok to use form here?
     document.getElementById('app').innerHTML = /*HTML*/`
+    <form id="registerUser">
+        <h3>Create an account</h3>
+        <p>
+            <label for="newUsernameInput">Username: </label>
+            <input type="text" id="newUsername" autofocus>
+        </p>
+
+        <p>
+            <label for="newPasswordInput">Password: </label>
+            <input type="text" id="newPassword">
+        </p>
+
+        <button type="submit" id="submitButton">Register</button>
+    </form>
     `;
 }
