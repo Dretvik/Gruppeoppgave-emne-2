e28b96f7718621d60d2079dcf605f6892ba433ca
3sph <141910502+3sph@users.noreply.github.com>
2023-10-23 13:51:38 +0200

    main page view

--- pages/mainPage/mainPageView.js
+++ pages/mainPage/mainPageView.js
@@ -1,4 +1,22 @@
 function mainPageView(){
+<<<<<<< Updated upstream
     document.getElementById('app').innerHTML = menuButtonAndSearchBar + /*HTML*/`
+=======
+    document.getElementById('app').innerHTML = /*HTML*/`
+    <div id="mainContainer">
+    <input type="radio" name="position" checked />
+    <input type="radio" name="position" />
+    <input type="radio" name="position" />
+    <input type="radio" name="position" />
+    <input type="radio" name="position" />
+    <main id="carousel">
+      <div class="item"><img src="${model.data.movies[0].cover}"></div>
+      <div class="item"><img src="${model.data.movies[1].cover}"></div>
+      <div class="item"><img src="${model.data.movies[2].cover}"></div>
+      <div class="item"><img src="${model.data.movies[3].cover}"></div>
+      <div class="item"><img src="${model.data.movies[4].cover}"></div>
+      </main>
+    </div>
+>>>>>>> Stashed changes
     `;
 }
