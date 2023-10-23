f81c358e603d93f9a7fe7ba081654ddb8099e28f
TorAlverMoen <tor.alver.moen@gmail.com>
2023-10-24 01:56:56 +0200

    Bug fix: Renamed the id in forgottenPageView.js
    This id is not used here but it’s still technically a bug.
    
    #jira GT3-10

--- pages/forgottenPage/forgottenPageView.js
+++ pages/forgottenPage/forgottenPageView.js
@@ -1,6 +1,6 @@
 function forgotPassword() {
     document.getElementById('app').innerHTML = /*HTML*/`
-    <div id="registerUser">
+    <div id="forgottenUserPassword">
         <h3>Forgot your password?</h3>
         <h4>Please note that this is not functional!</h4>
         <p>Please enter your email below so we can send you instructions to reset your password.</p>
