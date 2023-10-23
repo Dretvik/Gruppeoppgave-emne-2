227d3cb36c4382c8ee2d72120e22eddebaa41e21
ImValkyrie <bitte_1992@hotmail.com>
2023-10-23 13:15:58 +0200

    Added profilePageView
    #Jira GT3-17

---
+++ pages/profilePage/profilePageView.js
@@ -0,0 +1,27 @@
+function profilePageView () {
+const user = model.app.loggedInUser;
+document.getElementById('app').innerHTML = /*HTML*/`
+<div id="profilePageContainer">
+    <div class="profileInfo">
+        <img class="profilePicture" src="${user.profileImage || 'emptyUser.jpg'}" alt="Profile Picture">
+        <h2>Hi ${user.userName}! Welcome to your profile page.</h2>
+    </div>
+        <div id="profilePageInfo">
+        <h2>About ${user.userName}, ${user.aboutMe}</h2>
+    </div>
+    <div>
+        <h2> My favourite movies</h2>
+        <div id="favMoviesProfilePage"> ${user.favMovies}</div>
+    </div>
+    <div>
+        <h2>My rated movies</h2>
+        <div id="ratedMoviesProfilePage"> ${user.myRatedMovies}</div>
+    </div>
+    <div> 
+        <h2>Favourite genre</h2>
+        <div id="favGenreProfilePage"> ${user.favGenre}</div>
+    </div>
+
+</div>
+  `;
+}
