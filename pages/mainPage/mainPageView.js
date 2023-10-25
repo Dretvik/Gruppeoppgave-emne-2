ad0cb4db2bf26868b4051928a45f35e3d6214146
3sph <141910502+3sph@users.noreply.github.com>
2023-10-25 13:57:10 +0200

    main page view

--- pages/mainPage/mainPageView.js
+++ pages/mainPage/mainPageView.js
@@ -6,12 +6,22 @@ function mainPageView(){
     <input type="radio" name="position" />
     <input type="radio" name="position" />
     <input type="radio" name="position" />
+    <input type="radio" name="position" />
+    <input type="radio" name="position" />
+    <input type="radio" name="position" />
+    <input type="radio" name="position" />
+    <input type="radio" name="position" />
     <main id="carousel">
       <div class="item"><img src="${model.data.movies[0].cover}"></div>
       <div class="item"><img src="${model.data.movies[1].cover}"></div>
       <div class="item"><img src="${model.data.movies[2].cover}"></div>
       <div class="item"><img src="${model.data.movies[3].cover}"></div>
       <div class="item"><img src="${model.data.movies[4].cover}"></div>
+      <div class="item"><img src="${model.data.movies[5].cover}"></div>
+      <div class="item"><img src="${model.data.movies[6].cover}"></div>
+      <div class="item"><img src="${model.data.movies[7].cover}"></div>
+      <div class="item"><img src="${model.data.movies[8].cover}"></div>
+      <div class="item"><img src="${model.data.movies[9].cover}"></div>
       </main>
     </div>
 
