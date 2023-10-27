156874a914d8d51fefa9e47ff0162c257c9e080d
3sph <141910502+3sph@users.noreply.github.com>
2023-10-27 09:55:19 +0200

    opdatert karusell

--- pages/mainPage/mainPageView.js
+++ pages/mainPage/mainPageView.js
@@ -1,29 +1,46 @@
 function mainPageView(){
     document.getElementById('app').innerHTML = menuButtonAndSearchBar + /*HTML*/`
-    <div id="mainContainer">
+    <div id="mainContainer1">
     <input type="radio" name="position" checked />
     <input type="radio" name="position" />
     <input type="radio" name="position" />
     <input type="radio" name="position" />
     <input type="radio" name="position" />
-    <input type="radio" name="position" />
-    <input type="radio" name="position" />
-    <input type="radio" name="position" />
-    <input type="radio" name="position" />
-    <input type="radio" name="position" />
+   
+    
+    
     <main id="carousel">
       <div class="item"><img src="${model.data.movies[0].cover}"></div>
       <div class="item"><img src="${model.data.movies[1].cover}"></div>
       <div class="item"><img src="${model.data.movies[2].cover}"></div>
       <div class="item"><img src="${model.data.movies[3].cover}"></div>
       <div class="item"><img src="${model.data.movies[4].cover}"></div>
+      
+      
+     
+      </main>
+    </div>
+
+
+    <div id="mainContainer2">
+    <input type="radio" name="position2" checked />
+    <input type="radio" name="position2" />
+    <input type="radio" name="position2" />
+    <input type="radio" name="position2" />
+    <input type="radio" name="position2" />
+   
+    
+    
+    <main id="carousel2">
       <div class="item"><img src="${model.data.movies[5].cover}"></div>
       <div class="item"><img src="${model.data.movies[6].cover}"></div>
       <div class="item"><img src="${model.data.movies[7].cover}"></div>
       <div class="item"><img src="${model.data.movies[8].cover}"></div>
       <div class="item"><img src="${model.data.movies[9].cover}"></div>
+      
+      
+     
       </main>
     </div>
-
     `;
 }
