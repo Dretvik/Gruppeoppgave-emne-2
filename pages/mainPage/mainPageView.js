6a49bb2947074a274cff58c7203a6514db1dd102
Ellie Marie <orix@hotmail.no>
2023-11-01 13:09:57 +0100

    lagt til sortering på karusellene

--- pages/mainPage/mainPageView.js
+++ pages/mainPage/mainPageView.js
@@ -1,7 +1,13 @@
 function mainPageView(){
+  const moviesHigestRated = model.data.movies.slice(); 
+  moviesHigestRated.sort((a, b) => b.overallRating - a.overallRating)
+
+  const moviesReleaseDate = model.data.movies.slice();
+  moviesReleaseDate.sort((a, b) => b.releaseDate - a.releaseDate)
+
     document.getElementById('app').innerHTML = menuButtonAndSearchBar + /*HTML*/`
     <div id="mainContainer1">
-    <h1>top rated</h1>
+    <h1 id="h1MainPage">top rated</h1>
     <input type="radio" name="position" checked />
     <input type="radio" name="position" />
     <input type="radio" name="position" />
@@ -11,11 +17,11 @@ function mainPageView(){
     
     
     <main id="carousel">
-      <div class="item"><img src="${model.data.movies[0].cover}"></div>
-      <div class="item"><img src="${model.data.movies[1].cover}"></div>
-      <div class="item"><img src="${model.data.movies[2].cover}"></div>
-      <div class="item"><img src="${model.data.movies[3].cover}"></div>
-      <div class="item"><img src="${model.data.movies[4].cover}"></div>
+    <div class="item"><img src="${moviesHigestRated[0].cover}"><p>Overall rating: ${moviesHigestRated[0].overallRating}/1000</p></div>
+    <div class="item"><img src="${moviesHigestRated[1].cover}"><p>Overall rating: ${moviesHigestRated[1].overallRating}/1000</p></div>
+    <div class="item"><img src="${moviesHigestRated[2].cover}"><p>Overall rating: ${moviesHigestRated[2].overallRating}/1000</p></div>
+    <div class="item"><img src="${moviesHigestRated[3].cover}"><p>Overall rating: ${moviesHigestRated[3].overallRating}/1000</p></div>
+    <div class="item"><img src="${moviesHigestRated[4].cover}"><p>Overall rating: ${moviesHigestRated[4].overallRating}/1000</p></div>
       
       
      
@@ -24,7 +30,7 @@ function mainPageView(){
 
 
     <div id="mainContainer2">
-    <h2>fav movies</h2>
+    <h2 id="h2MainPage">fav movies</h2>
     <input type="radio" name="position2" checked />
     <input type="radio" name="position2" />
     <input type="radio" name="position2" />
@@ -34,11 +40,11 @@ function mainPageView(){
     
     
     <main id="carousel2">
-      <div class="item"><img src="${model.data.movies[5].cover}"></div>
-      <div class="item"><img src="${model.data.movies[6].cover}"></div>
-      <div class="item"><img src="${model.data.movies[7].cover}"></div>
-      <div class="item"><img src="${model.data.movies[8].cover}"></div>
-      <div class="item"><img src="${model.data.movies[9].cover}"></div>
+    <div class="item"><img src="${moviesReleaseDate[0].cover}"><p>Overall rating: ${moviesReleaseDate[0].overallRating}/1000</p></div>
+    <div class="item"><img src="${moviesReleaseDate[1].cover}"><p>Overall rating: ${moviesReleaseDate[1].overallRating}/1000</p></div>
+    <div class="item"><img src="${moviesReleaseDate[2].cover}"><p>Overall rating: ${moviesReleaseDate[2].overallRating}/1000</p></div>
+    <div class="item"><img src="${moviesReleaseDate[3].cover}"><p>Overall rating: ${moviesReleaseDate[3].overallRating}/1000</p></div>
+    <div class="item"><img src="${moviesReleaseDate[4].cover}"><p>Overall rating: ${moviesReleaseDate[4].overallRating}/1000</p></div>
       
       
      
