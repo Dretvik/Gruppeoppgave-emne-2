80e2425dea2f6f9765a7702685e7c6638459077e
Tor Alver Moen <tor.alver.moen@gmail.com>
2023-11-02 14:16:20 +0100

    Added CSS for the forgot password page
    #jira GT3-32

--- pages/forgottenPage/forgottenPageView.js
+++ pages/forgottenPage/forgottenPageView.js
@@ -1,17 +1,16 @@
 function forgotPassword() {
     document.getElementById('app').innerHTML = /*HTML*/`
-    <div id="forgottenUserPassword">
+    <div class="forgottenUserPassword">
         <h3>Forgot your password?</h3>
-        <h4>Please note that this is not functional!</h4>
-        <p>Please enter your email below so we can send you instructions to reset your password.</p>
-
+        <p>Please enter your email below so we can send you instructions to reset your password.<br>
+        (Please note that this is not functional so no e-mail will be sent!)</p>
         <p>
             <label for="forgottenUserPasswordEmail">E-mail: </label>
             <input type="email" id="forgottenPasswordEmail">
         </p>
-
-        <button onclick="forgottenPassword()" id="forgottenButton">Send e-mail</button>
-        <button onclick="loginPageView()">Back</button>
     </div>
+
+    <button onclick="forgottenPassword()" id="forgottenButton">Send e-mail</button>
+    <button onclick="loginPageView()" id="forgottenBackButton">Back</button>
     `;
 }
