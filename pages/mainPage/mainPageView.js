bdd7c193018c4c7acf381153e39c57fec45bee55
3sph <141910502+3sph@users.noreply.github.com>
2023-11-07 14:56:56 +0100

    har ordnet karusell nmr 3, med funsk og plasering av knapper.

--- pages/mainPage/mainPageView.js
+++ pages/mainPage/mainPageView.js
@@ -2,6 +2,9 @@ function mainPageView(){
   const moviesHigestRated = model.data.movies.slice(); 
   moviesHigestRated.sort((a, b) => b.overallRating - a.overallRating)
 
+  const moviesAlphabetic = model.data.movies.slice();
+  moviesAlphabetic.sort ((a, b) => a.title.localeCompare(b.title))
+
   const moviesReleaseDate = model.data.movies.slice();
   moviesReleaseDate.sort((a, b) => b.releaseDate - a.releaseDate)
 
@@ -28,9 +31,10 @@ function mainPageView(){
       </main>
     </div>
 
+// second carousel
 
     <div id="mainContainer2">
-    <h2 id="h2MainPage">Fav movies</h2>
+    <h2 id="h2Realese Date">Fav movies</h2>
     <input type="radio" name="position2" checked />
     <input type="radio" name="position2" />
     <input type="radio" name="position2" />
@@ -45,6 +49,28 @@ function mainPageView(){
     <div class="item" onclick="movieInfoPageView(${moviesReleaseDate[2].id});"><img src="${moviesReleaseDate[2].cover}"><p>Overall rating: ${moviesReleaseDate[2].overallRating}/1000</p></div>
     <div class="item" onclick="movieInfoPageView(${moviesReleaseDate[3].id});"><img src="${moviesReleaseDate[3].cover}"><p>Overall rating: ${moviesReleaseDate[3].overallRating}/1000</p></div>
     <div class="item" onclick="movieInfoPageView(${moviesReleaseDate[4].id});"><img src="${moviesReleaseDate[4].cover}"><p>Overall rating: ${moviesReleaseDate[4].overallRating}/1000</p></div>
+      
+      
+     
+      </main>
+    </div>
+    <div id="mainContainer3">
+<main id="carousel3">
+<h3 id="h3MainPage"></h3>
+    <input type="radio" name="position3" checked />
+    <input type="radio" name="position3" />
+    <input type="radio" name="position3" />
+    <input type="radio" name="position3" />
+    <input type="radio" name="position3" />
+   
+    
+    
+    <main id="carousel3">
+    <div class="item" onclick="movieInfoPageView(${moviesAlphabetic[0].id});"><img src="${moviesAlphabetic[0].cover}"><p>Overall rating: ${moviesHigestRated[0].overallRating}/1000</p></div>
+    <div class="item" onclick="movieInfoPageView(${moviesAlphabetic[1].id});"><img src="${moviesAlphabetic[1].cover}"><p>Overall rating: ${moviesHigestRated[1].overallRating}/1000</p></div>
+    <div class="item" onclick="movieInfoPageView(${moviesAlphabetic[2].id});"><img src="${moviesAlphabetic[2].cover}"><p>Overall rating: ${moviesHigestRated[2].overallRating}/1000</p></div>
+    <div class="item" onclick="movieInfoPageView(${moviesAlphabetic[3].id});"><img src="${moviesAlphabetic[3].cover}"><p>Overall rating: ${moviesHigestRated[3].overallRating}/1000</p></div>
+    <div class="item" onclick="movieInfoPageView(${moviesAlphabetic[4].id});"><img src="${moviesAlphabetic[4].cover}"><p>Overall rating: ${moviesHigestRated[4].overallRating}/1000</p></div>
       
       
      
